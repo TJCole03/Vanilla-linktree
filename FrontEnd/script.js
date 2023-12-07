4e3d1433dac28ab5a15fb25fe827d4fc07e769b2
TJCole03 <tjc6254@gmail.com>
2023-12-07 07:12:58 -0500

    styling- got links verticalized, working on alignment and padding. colors and aesthetics to come later

---
+++ FrontEnd/script.js
@@ -0,0 +1,11 @@
+/* DOM */
+const bookAClass = document.getElementById('link1')
+const viewClasses = document.getElementById('link2')
+const aboutMe = document.getElementById('link3')
+const shop = document.getElementById('link4')
+const contactMe = document.getElementById('link5')
+
+
+const followLink = link1.addEventListener('click', () => {
+    console.log('clicked')
+})
