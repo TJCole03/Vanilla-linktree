8047dd10706d5fc7df557a42d3d449c468bc754f
TJCole03 <tjc6254@gmail.com>
2023-12-12 07:13:04 -0500

    added email sign up option and submit button and connected it to the DOM

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -4,8 +4,13 @@ const viewClasses = document.getElementById('link2')
 const aboutMe = document.getElementById('link3')
 const shop = document.getElementById('link4')
 const contactMe = document.getElementById('link5')
+const emailList = document.getElementById('submit')
 
 
 const followLink = link1.addEventListener('click', () => {
+    console.log('clicked')
+})
+
+const signUpForEmail = submit.addEventListener('click', () => {
     console.log('clicked')
 })
