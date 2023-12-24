bb564d00d7e8a57cacd81f74207504ff1e571b2d
TJCole03 <tjc6254@gmail.com>
2023-12-24 07:06:56 -0500

    centered all divs and began creating pop-up modal for users to add their names to the email list. Will need to add some backend in there as well

---
+++ Main/script.js
@@ -0,0 +1,26 @@
+/* DOM */
+const bookAClass = document.getElementById('link1')
+const viewClasses = document.getElementById('link2')
+const aboutMe = document.getElementById('link3')
+const shop = document.getElementById('link4')
+const contactMe = document.getElementById('link5')
+const emailList = document.getElementById('clickHere')
+
+//when you click on email list, I want there to be a modal for people to sign up for mysticbrew email
+//have to make module in html and then render it to the DOM!
+
+
+function signUpModule(signUp) {
+    if (signUp) {
+        return alert('your thing worked, dumbass')
+    }
+    signUpModule()
+}
+
+const signUp = clickHere.addEventListener('click', () => {
+    return module("Module?")
+})
+
+// const followLink = link1.addEventListener('click', () => {
+    //     console.log('clicked')
+    // })
